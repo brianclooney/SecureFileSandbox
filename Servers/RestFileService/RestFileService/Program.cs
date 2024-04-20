6ef7acfb51757ee8169942ab5bedc1291056bb75
Brian Clooney <bclooney18@gmail.com>
2024-04-19 22:16:23 -0400

    Integrate Serilog for enhanced application logging and configure request logging

--- Servers/RestFileService/RestFileService/Program.cs
+++ Servers/RestFileService/RestFileService/Program.cs
@@ -1,9 +1,20 @@
 using Carter;
 using FluentValidation;
+using Serilog;
+using Serilog.Events;
 using Swashbuckle.AspNetCore.Swagger;
 using RestFileService.Features.Users;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning) 
+    .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u1} {Message}{NewLine}{Exception}")
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddCarter();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
@@ -21,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.MapCarter();
 
